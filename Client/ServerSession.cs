e71ca1e551ab4143355bd11de8e36da778de8a16
Han sanghyun <hshf123@naver.com>
2022-02-10 22:54:19 +0900

    List<Struct> Packet Serializtion

--- Client/ServerSession.cs
+++ Client/ServerSession.cs
@@ -26,6 +26,39 @@ namespace Client
         public long userId;
         public string userName;
 
+        public struct TestInfo
+        {
+            public int testInt;
+            public short testShort;
+            public float testFloat;
+
+            public bool Write(Span<byte> span, ref ushort count)
+            {
+                bool success = true;
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testInt);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testShort);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testFloat);
+                count += sizeof(float);
+
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> read, ref ushort count)
+            {
+                testInt = BitConverter.ToInt32(read.Slice(count, read.Length - count));
+                count += sizeof(int);
+                testShort = BitConverter.ToInt16(read.Slice(count, read.Length - count));
+                count += sizeof(short);
+                testFloat = BitConverter.ToSingle(read.Slice(count, read.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<TestInfo> list = new List<TestInfo>();
+
         public UserInfoReq()
         {
             this.packetId = (ushort)PacketID.UserID;
@@ -40,9 +73,21 @@ namespace Client
             count += sizeof(ushort);
             this.userId = BitConverter.ToInt64(read.Slice(count, read.Length - count));
             count += sizeof(long);
+
             ushort userNameLen = BitConverter.ToUInt16(read.Slice(count, read.Length - count));
             count += sizeof(ushort);
             this.userName = Encoding.Unicode.GetString(read.Slice(count, userNameLen));
+            count += userNameLen;
+
+            ushort listLen = BitConverter.ToUInt16(read.Slice(count, read.Length - count));
+            count += sizeof(ushort);
+            list.Clear();
+            for (int i = 0; i < listLen; i++)
+            {
+                TestInfo testInfo = new TestInfo();
+                testInfo.Read(read, ref count);
+                list.Add(testInfo);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -65,7 +110,10 @@ namespace Client
             Array.Copy(Encoding.Unicode.GetBytes(this.userName), 0, segment.Array, count, userNameLen);
             count += userNameLen;
 
-            //ushort userNameLen = (ushort)Encoding.Unicode.GetBytes(this.userName, 0, this.userName.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)list.Count);
+            count += sizeof(ushort);
+            foreach (TestInfo testInfo in list)
+                testInfo.Write(span, ref count);
 
             success &= BitConverter.TryWriteBytes(span, count);
 
@@ -87,6 +135,12 @@ namespace Client
                 {
                     // Send
                     UserInfoReq packet = new UserInfoReq() { userId = 1798, userName = "LeafC" };
+                    for (int i = 0; i < 4; i++)
+                    {
+                        UserInfoReq.TestInfo testInfo = new UserInfoReq.TestInfo() { testInt = (i + 1) * 1000, testShort = (short)i, testFloat = 1.5f };
+                        packet.list.Add(testInfo);
+                    }
+
 
                     ArraySegment<byte> segment = packet.Write();
                     if (segment != null)
@@ -108,7 +162,7 @@ namespace Client
 
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
-            
+
         }
 
         public override void OnSend(int numOfBytes)
--- Server/ClientSession.cs
+++ Server/ClientSession.cs
@@ -25,6 +25,39 @@ namespace Server
         public long userId;
         public string userName;
 
+        public struct TestInfo
+        {
+            public int testInt;
+            public short testShort;
+            public float testFloat;
+
+            public bool Write(Span<byte> span, ref ushort count)
+            {
+                bool success = true;
+
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testInt);
+                count += sizeof(int);
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testShort);
+                count += sizeof(short);
+                success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), testFloat);
+                count += sizeof(float);
+
+                return success;
+            }
+
+            public void Read(ReadOnlySpan<byte> read, ref ushort count)
+            {
+                testInt = BitConverter.ToInt32(read.Slice(count, read.Length - count));
+                count += sizeof(int);
+                testShort = BitConverter.ToInt16(read.Slice(count, read.Length - count));
+                count += sizeof(short);
+                testFloat = BitConverter.ToSingle(read.Slice(count, read.Length - count));
+                count += sizeof(float);
+            }
+        }
+
+        public List<TestInfo> list = new List<TestInfo>();
+
         public UserInfoReq()
         {
             this.packetId = (ushort)PacketID.UserID;
@@ -39,9 +72,21 @@ namespace Server
             count += sizeof(ushort);
             this.userId = BitConverter.ToInt64(read.Slice(count, read.Length - count));
             count += sizeof(long);
+
             ushort userNameLen = BitConverter.ToUInt16(read.Slice(count, read.Length - count));
             count += sizeof(ushort);
             this.userName = Encoding.Unicode.GetString(read.Slice(count, userNameLen));
+            count += userNameLen;
+
+            ushort listLen = BitConverter.ToUInt16(read.Slice(count, read.Length - count));
+            count += sizeof(ushort);
+            list.Clear();
+            for (int i = 0; i < listLen; i++)
+            {
+                TestInfo testInfo = new TestInfo();
+                testInfo.Read(read, ref count);
+                list.Add(testInfo);
+            }
         }
 
         public override ArraySegment<byte> Write()
@@ -64,7 +109,10 @@ namespace Server
             Array.Copy(Encoding.Unicode.GetBytes(this.userName), 0, segment.Array, count, userNameLen);
             count += userNameLen;
 
-            //ushort userNameLen = (ushort)Encoding.Unicode.GetBytes(this.userName, 0, this.userName.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+            success &= BitConverter.TryWriteBytes(span.Slice(count, span.Length - count), (ushort)list.Count);
+            count += sizeof(ushort);
+            foreach (TestInfo testInfo in list)
+                testInfo.Write(span, ref count);
 
             success &= BitConverter.TryWriteBytes(span, count);
 
@@ -93,13 +141,15 @@ namespace Server
             ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
             count += sizeof(ushort);
 
-            switch((PacketID)id)
+            switch ((PacketID)id)
             {
                 case PacketID.UserID:
                     {
                         UserInfoReq packet = new UserInfoReq();
                         packet.Read(buffer);
                         Console.WriteLine($"UserInfoReq : {packet.userId}, UserName : {packet.userName}");
+                        foreach (UserInfoReq.TestInfo test in packet.list)
+                            Console.WriteLine($"Int : {test.testInt} Short : {test.testShort} Float : {test.testFloat}");
                     }
                     break;
             }
